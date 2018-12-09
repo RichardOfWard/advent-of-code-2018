db60440a91ac65639a49dce5191709fe9441a133
Richard Ward <richard@richard.ward.name>
2018-12-09 11:31:26 +0000

    09.2 prep: speed from 1m22s to 1.65s

--- src/Day09.py
+++ src/Day09.py
@@ -10,22 +10,14 @@ def play_game(num_players, largest_marble):
     scores = [0] * num_players
     board = [0]
     current_marble_index = 0
-    next_player = 1
     for next_marble in range(1, largest_marble + 1):
-        next_marble_index = (current_marble_index + 2) % len(board)
-        if next_marble_index == 0:
-            next_marble_index = len(board)
-
         if next_marble % 23 != 0:
-            board = board[:next_marble_index] + [next_marble] + board[next_marble_index:]
-            current_marble_index = next_marble_index
+            current_marble_index = (current_marble_index + 2) % len(board)
+            board.insert(current_marble_index, next_marble)
         else:
-            removal_index = (current_marble_index - 7) % len(board)
-            scores[next_player] += next_marble + board[removal_index]
-            board = board[:removal_index - 1] + board[removal_index:]
-            current_marble_index = removal_index
+            current_marble_index = (current_marble_index - 7) % len(board)
+            scores[next_marble % num_players] += next_marble + board.pop(current_marble_index)
 
-        next_player = (next_player + 1) % num_players
     return scores
 
 
